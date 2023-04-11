c21d2c6564379946d3e5ed0ed60cf58ffaf35f90
sato_minami <sato_minami@192.168.130.172>
2023-04-11 22:03:58 +0900

    task2

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -1,5 +1,7 @@
 package jp.co.seattle.library.controller;
 
+ 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,38 +14,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.UsersService;
 
+ 
+
 /**
  * ログインコントローラー
  */
 @Controller /** APIの入り口 */
 public class LoginController {
-	final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    final static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	@Autowired
-	private UsersService usersService;
+ 
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String first(Model model) {
-		return "login"; // jspファイル名
-	}
+    @Autowired
+    private UsersService usersService;
 
-	/**
-	 * ログイン処理
-	 *
-	 * @param email    メールアドレス
-	 * @param password パスワード
-	 * @param model
-	 * @return ホーム画面に遷移
-	 */
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+ 
 
-		// メアドとパスワードに一致するユーザー取得
-		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String first(Model model) {
+        return "login"; // jspファイル名
+    }
+    
+    
+    /**
+     * ログイン処理
+     *
+     * @param email    メールアドレス
+     * @param password パスワード
+     * @param model
+     * @return ホーム画面に遷移
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
 
-		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
+ 
 
-		
-		return "redirect:/home";
-	}
+        // メアドとパスワードに一致するユーザー取得
+        UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
+
+ 
+
+        // ユーザーが存在すればログイン、存在しなければエラー(タスク２)
+        if (selectedUserInfo != null) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
+            return "login";
+        }
+    }
 }
